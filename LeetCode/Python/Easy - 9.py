8a791aa0084949aeb968e11fdacbcf4f4ee29563
Brynja Schultz <54891716+brynja-schultz@users.noreply.github.com>
2026-09-25 13:32:20 +0200

    Add binary search implementation in Easy - 9.py
    Implement binary search algorithm to find target index.

---
+++ LeetCode/Python/Easy - 9.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = ((right - left)//2) + left
+
+            if target == nums[mid]:
+                return mid
+
+            elif target < nums[mid]:
+                right = mid - 1
+
+            elif target > nums[mid]:
+                left = mid + 1
+
+        return -1
